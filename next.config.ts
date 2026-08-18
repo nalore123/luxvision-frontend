638991f883a5d4ce0fc976d6d4e5d2627b224843
nalore123 <kosiclorena@gmail.com>
2026-08-18 13:26:05 +0200

    Promjena

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,11 @@ const nextConfig: NextConfig = {
         port: "8000",
         pathname: "/media/**",
       },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
     ],
   },
 };
